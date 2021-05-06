8e983ffada2f7404b40fee23f34e4fdb0a011b17
Mikkel Wittenburg Petersen <mikkel.w.petersen@gmail.com>
2021-05-06 14:10:31 +0200

    added plot with change in % membership overall and plot with % membership grouped by age group

--- shiny-app-membership-national-church.R
+++ shiny-app-membership-national-church.R
@@ -31,19 +31,38 @@ municipality_list <- unique(df$KOMK)
 
 ui <- fluidPage(
   selectInput("municipality", "Choose municipality", choices = municipality_list),
-  plotOutput("plot")
+  plotOutput("plot_change"),
+  plotOutput("plot_age")
 )
 
 server <- function(input, output, session) {
-  p <- reactive(df %>%
-                  filter(KOMK == input$municipality, 
-                         FKMED == "Member of National Church") %>%
-                  group_by(TID) %>%
-                  summarise(total_members = sum(INDHOLD)) %>%
-                  ggplot(., aes(TID, total_members)) +
+  p_change <- reactive(df %>%
+                  filter(KOMK == input$municipality) %>%
+                  group_by(TID, FKMED) %>%
+                  summarise(total = sum(INDHOLD)) %>%
+                  mutate(percent = round(total/sum(total) * 100, 1)) %>%
+                  filter(FKMED == "Member of National Church") %>%
+                  ggplot(., aes(TID, percent)) +
                   geom_line())
   
-  output$plot <- renderPlot(p(), res = 96, alt = "Alternative text")
+  age_levels <- unique(df$ALDER)
+  
+  p_age <- reactive(df %>%
+                  filter(TID == max(TID),
+                         KOMK == input$municipality) %>%
+                  group_by(ALDER, FKMED) %>%
+                  summarise(total = sum(INDHOLD)) %>%
+                  mutate(percent = round(total/sum(total) * 100, 1)) %>%
+                  filter(FKMED == "Member of National Church") %>%
+                  mutate(ALDER = factor(ALDER, levels = age_levels)) %>%
+                  arrange(ALDER) %>%
+                  ggplot(., aes(ALDER, percent)) +
+                  geom_col() +
+                  coord_flip())
+  
+  output$plot_change <- renderPlot(p_change(), res = 96, alt = "Alternative text")
+  
+  output$plot_age <- renderPlot(p_age(), res = 96, alt = "Alternative text")
 }
 
 shinyApp(ui, server)
